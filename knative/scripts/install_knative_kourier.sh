9d7c7ca2e7fc9982a92e3f4820f2e9347a6e243e
erasmodominguezdc <erasmo_dominguez@discovery.com>
2021-07-24 09:27:23 +0100

    istio installation

--- knative/scripts/install_knative_kourier.sh
+++ knative/scripts/install_knative_kourier.sh
@@ -5,6 +5,9 @@
 ## SEE https://knative.dev/blog/1/01/01/how-to-set-up-a-local-knative-environment-with-kind-and-without-dns-headaches/#step-3-set-up-networking-using-kourier
 ## curl -Lo kourier.yaml https://github.com/knative-sandbox/net-kourier/releases/download/v0.15.0/kourier.yaml
 echo 'Installing kourier'
+
+export KNATIVE_SERVING_VERSION="0.24.0"
+
 kubectl create namespace kourier-system
 
 kubectl apply --filename ./config/kourier.yaml
