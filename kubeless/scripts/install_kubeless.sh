b1bf858b34b3cad210081e8641e3b847df00541d
erasmodominguezdc <erasmo_dominguez@discovery.com>
2021-07-29 08:55:24 +0100

    kubeless

--- kubeless/scripts/install_kubeless.sh
+++ kubeless/scripts/install_kubeless.sh
@@ -3,14 +3,13 @@
 export RELEASE=$(curl -s https://api.github.com/repos/kubeless/kubeless/releases/latest | grep tag_name | cut -d '"' -f 4)
 
 kubectl create namespace kubeless
+
 kubectl create -f https://github.com/kubeless/kubeless/releases/download/$RELEASE/kubeless-$RELEASE.yaml
-
-kubectl get customresourcedefinition
-
-kubeless get-server-config
 
 kubectl create -f https://raw.githubusercontent.com/kubeless/kubeless-ui/master/k8s.yaml
 
 kubectl get pods -n kubeless
 
 kubectl get deployment -n kubeless
+
+kubectl get customresourcedefinition
